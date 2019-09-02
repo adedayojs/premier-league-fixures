360c15508054280d7703d756d238b0ff608d9604
adedayojs <samfeolu@gmail.com>
2019-09-03 00:14:22 +0100

    Passed All failing test except creating team

---
+++ routes/teams/controller.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+function createTeam(req: express.Request, res: express.Response) {
+  res.end();
+}
+function viewTeam(req: express.Request, res: express.Response) {
+  res.end();
+}
+function editTeam(req: express.Request, res: express.Response) {
+  res.end();
+}
+function deleteTeam(req: express.Request, res: express.Response) {
+  res.end();
+}
+export { createTeam, viewTeam, editTeam, deleteTeam };
