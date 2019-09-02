01756c94a664d6625b8fbaac9bf93fc187196328
adedayojs <samfeolu@gmail.com>
2019-09-03 00:20:41 +0100

    Added more test to team creation

--- routes/teams/__tests__/controller.ts
+++ routes/teams/__tests__/controller.ts
@@ -6,4 +6,20 @@ describe('POST ENDPOINT', () => {
     const res = await request(app).post('/api/v1/teams');
     expect(res.status).not.toBe(404);
   });
+
+  it('Should return error if no data is sent', async () => {
+    const res = await request(app).post('/api/v1/teams');
+    expect(res.status).toBe(400);
+  });
+
+  it('Should return error if invalid data is sent', async () => {
+    const res = await request(app)
+      .post('/api/v1/teams')
+      .send({
+        name: 'Manchester United',
+        manager: 'Adedayo ',
+        league: 'EPL'
+      });
+    expect(res.status).toBe(400);
+  });
 });
