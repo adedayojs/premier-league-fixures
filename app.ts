94a1d401b5552f34c706962f9393109d1aed54dd
adedayojs <samfeolu@gmail.com>
2019-09-02 20:48:06 +0100

    Updated the user model

--- app.ts
+++ app.ts
@@ -1,8 +1,11 @@
-import createError from 'http-errors'
-import express from  'express'
-import  path  from 'path'
-import cookieParser from 'cookie-parser'
+import createError from 'http-errors';
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
@@ -19,6 +22,31 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+let env = `${process.env.NODE_ENV}`;
+
+env = process.env.NODE_ENV || 'DEVELOPMENT';
+
+let envString = env.toUpperCase();
+
+// Connection to mongoDB
+
+const uri = `${process.env[`ATLAS_URI_${envString}`]}`;
+
+mongoose.set('useFindAndModify', false);
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully!');
+});
+connection.once('open', () => {});
+connection.on('error', () => {
+  console.log('Error Connecting To Database');
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -28,7 +56,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err:any, req:express.Request, res:express.Response, next:express.NextFunction) {
+app.use(function(err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -39,4 +67,4 @@ app.use(function(err:any, req:express.Request, res:express.Response, next:expres
 });
 
 // export default app;
-module.exports = app
+module.exports = app;
