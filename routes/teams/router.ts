163bf2029814ed25984cd7aec91ada3e318ad055
adedayojs <samfeolu@gmail.com>
2019-09-03 18:14:15 +0100

    Finished all CRUD routes on teams

--- routes/teams/router.ts
+++ routes/teams/router.ts
@@ -3,5 +3,8 @@ import { createTeam, deleteTeam, editTeam, viewTeam } from './controller';
 const router = Router();
 
 router.post('/', createTeam);
+router.get('/', viewTeam);
+router.put('/:id', editTeam);
+router.delete('/:id', deleteTeam);
 
 export default router;
