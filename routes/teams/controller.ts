58ebf7e8ffda5dc6143cee8acd7f61a2b153229e
adedayojs <samfeolu@gmail.com>
2019-09-03 10:34:39 +0100

    Added Joi validation

--- routes/teams/controller.ts
+++ routes/teams/controller.ts
@@ -1,8 +1,14 @@
 import express from 'express';
 import Team from '../../models/team';
+import {validateTeam} from '../../helpers/joi-validator'
 
 async function createTeam(req: express.Request, res: express.Response) {
   // Remember to add Joi Validation when internet comes up
+  const {error} = validateTeam(req.body)
+  if(error) {
+    res.status(400).json(error)
+    return
+  }
   const team = await new Team(req.body).save().catch(err => {
     res.status(400).json(err.message);
     return;
