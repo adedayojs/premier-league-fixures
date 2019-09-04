b889e6907d9bed25280618e136f78a84cb1e0c09
adedayojs <samfeolu@gmail.com>
2019-09-04 03:56:35 +0100

    Implemented Authorization for Create Team Route

--- routes/teams/router.ts
+++ routes/teams/router.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { createTeam, deleteTeam, editTeam, viewTeam } from './controller';
+import { validateAdminRoutes } from '../../middlewares/routesTokenValidator';
 const router = Router();
 
-router.post('/', createTeam);
+router.post('/', validateAdminRoutes, createTeam);
 router.get('/', viewTeam);
 router.put('/:id', editTeam);
 router.delete('/:id', deleteTeam);
