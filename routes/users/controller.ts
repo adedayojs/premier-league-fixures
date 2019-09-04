7239ab69487614d66841d26d264cf1a4ca0fa9bf
adedayojs <samfeolu@gmail.com>
2019-09-04 02:21:17 +0100

    Implemented password hash upon creating user

--- routes/users/controller.ts
+++ routes/users/controller.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import User, { IUser } from '../../models/user';
 import { validateUser, validateLogin } from '../../helpers/joi-validator';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 async function createUser(req: express.Request, res: express.Response) {
   /*  Check incoming data if it matches required data and return error
       if it doesn't match required data
@@ -10,6 +12,13 @@ async function createUser(req: express.Request, res: express.Response) {
     res.status(400).json(error);
     return;
   }
+
+  /* Hash Password before storing into database*/
+  const saltRounds = 16;
+  const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+  req.body.password = hashedPassword;
+
+  /*Store Data Into database*/
   const user = await new User(req.body).save().catch(err => {
     res.status(400).json(err.message);
     return;
@@ -18,5 +27,11 @@ async function createUser(req: express.Request, res: express.Response) {
   res.status(201).json(user);
 }
 
-async function logUserIn(req: express.Request, res: express.Response) {}
+async function logUserIn(req: express.Request, res: express.Response) {
+  const { error } = validateLogin(req.body);
+  if (error) {
+    res.status(400).json(error);
+    return;
+  }
+}
 export { createUser, logUserIn };
