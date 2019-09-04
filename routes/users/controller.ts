d352c8a893a7f4fef70cabb851051fd91dd218d4
adedayojs <samfeolu@gmail.com>
2019-09-04 03:01:12 +0100

    Implemented Password compare and token assignment upon login

--- routes/users/controller.ts
+++ routes/users/controller.ts
@@ -14,10 +14,9 @@ async function createUser(req: express.Request, res: express.Response) {
   }
 
   /* Hash Password before storing into database*/
-  const saltRounds = 16;
+  const saltRounds = parseInt(`${process.env.SALT_ROUNDS}`);
   const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
   req.body.password = hashedPassword;
-
   /*Store Data Into database*/
   const user = await new User(req.body).save().catch(err => {
     res.status(400).json(err.message);
@@ -36,10 +35,24 @@ async function logUserIn(req: express.Request, res: express.Response) {
     res.status(400).json(error);
     return;
   }
-  /* Check if user exist in database*/
+  /* Check if user exist in database, return not found (404) if user does not exists*/
   const user = await User.findOne({ email: req.body.email });
-  if(!user){
-    return 
+  if (!user) {
+    res.status(404).json({ error: 'User Does Not Exist in Our Database' });
+    return;
   }
+
+  /* If User Exists compare the password with the existing one in the database*/
+  const match = await bcrypt.compare(req.body.password, user.password);
+  if (!match) {
+    res.status(400).json({ error: 'Wrong Password Supplied' });
+    return;
+  }
+
+  /* If password matches, create a token for the said user and send it back*/
+  const secretKey = `${process.env.JWT_LOGIN_TOKEN}`;
+  const payload = req.body;
+  const token = jwt.sign(payload, secretKey);
+  res.json({ token });
 }
 export { createUser, logUserIn };
