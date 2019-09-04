7325afd3989bfdd925b3c1a70fa64f3484d97e65
adedayojs <samfeolu@gmail.com>
2019-09-04 01:31:02 +0100

    Linked Fixture controller to router for PUT endpoint

--- routes/fixtures/router.ts
+++ routes/fixtures/router.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { createFixture, viewFixtures, editFixture } from './controller';
+import { createFixture, viewFixtures, editFixture, deleteFixture } from './controller';
 const router = Router();
 
 router.post('/', createFixture);
 router.get('/', viewFixtures);
 router.put('/:id', editFixture);
+router.delete('/:id', deleteFixture);
 
 export default router;
