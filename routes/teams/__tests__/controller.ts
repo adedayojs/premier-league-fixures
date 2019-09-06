2478f8094eb51e6b38df0a4e131921e5d280762b
adedayojs <samfeolu@gmail.com>
2019-09-06 17:17:15 +0100

    Changed Team test file to use real time data to test

--- routes/teams/__tests__/controller.ts
+++ routes/teams/__tests__/controller.ts
@@ -12,6 +12,7 @@ describe('POST ENDPOINT', () => {
   afterAll(async () => {
     await Team.deleteMany({ name: 'Manchester United' });
   });
+
   it('Should be defined', async () => {
     const res = await request(app).post('/api/v1/teams');
     expect(res.status).not.toBe(404);
@@ -46,6 +47,7 @@ describe('POST ENDPOINT', () => {
       });
     expect(res.status).toBe(400);
   });
+
   it('Should return response 201 if valid data is sent', async () => {
     const res = await request(app)
       .post('/api/v1/teams')
@@ -54,12 +56,12 @@ describe('POST ENDPOINT', () => {
         'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNhbWZlb2x1QGdtYWlsLmNvbSIsInBhc3N3b3JkIjoic29tIiwicm9sZSI6IkFETUlOIiwiaWF0IjoxNTY3NTY0MDU5fQ.oj8nb4EOW2ZH7WheIcna0L_s2IM9hOeu46JHRZy5LV0'
       )
       .send({
-        name: 'Manchester United',
+        name: 'Manchester Random United',
         manager: 'Adedayo ',
         league: 'EPL',
         fixtures: [
-          { team: 'Chelsea Fc', date: new Date(2019, 3, 15), location: 'home' },
-          { team: 'Arsenal Fc', date: new Date(2019, 4, 15), location: 'home' }
+          { team: '5d4155cfcd68f4086d8df500', date: new Date(2019, 3, 15), location: 'home' },
+          { team: '5d4155cfcd68f4086d8df505', date: new Date(2019, 4, 15), location: 'home' }
         ]
       });
     expect(res.status).toBe(201);
@@ -70,28 +72,15 @@ describe('GET ENDPOINT', () => {
   afterAll(async () => {
     await Team.deleteMany({ league: 'EPL' });
   });
-  // it('should be defined', async () => {
-  //   const team = await new Team({
-  //     name: 'Manchester City',
-  //     manager: 'Adedayo ',
-  //     league: 'EPL',
-  //     fixtures: [
-  //       { team: 'Chelsea Fc', date: new Date(2019, 3, 15), location: 'home' },
-  //       { team: 'Arsenal Fc', date: new Date(2019, 4, 15), location: 'home' }
-  //     ]
-  //   }).save();
-  //   const res = await request(app).get('/api/v1/teams');
-  //   expect(res.status).not.toBe(404);
-  // });
 
   it('should return a created team', async () => {
     const team = await new Team({
-      name: 'Manchester United',
+      name: 'Random United',
       manager: 'Aded ',
       league: 'EPL',
       fixtures: [
-        { team: 'Chelsea Fc', date: new Date(2019, 3, 15), location: 'home' },
-        { team: 'Arsenal Fc', date: new Date(2019, 4, 15), location: 'home' }
+        { team: '5d4155cfcd68f4086d8df500', date: new Date(2019, 3, 15), location: 'home' },
+        { team: '5d4155cfcd68f4086d8df500', date: new Date(2019, 4, 15), location: 'home' }
       ]
     }).save();
     const res = await request(app).get('/api/v1/teams');
@@ -99,36 +88,18 @@ describe('GET ENDPOINT', () => {
       'data',
       expect.arrayContaining([expect.objectContaining({ name: team.name })])
     );
-    // expect(res.body).toEqual(expect.objectContaining({data:expect.arrayContaining([expect.objectContaining({ name: team.name })])}));
   });
 });
 
 describe('PUT ENDPOINT', () => {
-  let team: ITeam;
-  beforeAll(async () => {
-    team = await new Team({
-      name: 'Manchester United',
-      manager: 'Adedayo ',
-      league: 'EPL',
-      fixtures: [
-        { team: 'Chelsea Fc', date: new Date(2019, 3, 15), location: 'home' },
-        { team: 'Arsenal Fc', date: new Date(2019, 4, 15), location: 'home' }
-      ]
-    }).save();
-  });
-
-  afterAll(async () => {
-    await Team.deleteMany({ name: 'Manchester United' });
-  });
-
   it('Should be defined', async () => {
-    const res = await request(app).put(`/api/v1/teams/${team._id}`);
+    const res = await request(app).put(`/api/v1/teams/5d4155cfcd68f4086d8df504`);
     expect(res.status).not.toBe(404);
   });
 
   it('Should be return bad request if any required field is not present', async () => {
     const res = await request(app)
-      .put(`/api/v1/teams/${team._id}`)
+      .put(`/api/v1/teams/5d4155cfcd68f4086d8df504`)
       .set(
         'Authorization',
         'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNhbWZlb2x1QGdtYWlsLmNvbSIsInBhc3N3b3JkIjoic29tIiwicm9sZSI6IkFETUlOIiwiaWF0IjoxNTY3NTY0MDU5fQ.oj8nb4EOW2ZH7WheIcna0L_s2IM9hOeu46JHRZy5LV0'
@@ -139,19 +110,19 @@ describe('PUT ENDPOINT', () => {
 
   it('Should be return success if all required fields are present', async () => {
     const res = await request(app)
-      .put(`/api/v1/teams/${team._id}`)
+      .put(`/api/v1/teams/5d4155cfcd68f4086d8df504`)
       .set(
         'Authorization',
         'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNhbWZlb2x1QGdtYWlsLmNvbSIsInBhc3N3b3JkIjoic29tIiwicm9sZSI6IkFETUlOIiwiaWF0IjoxNTY3NTY0MDU5fQ.oj8nb4EOW2ZH7WheIcna0L_s2IM9hOeu46JHRZy5LV0'
       )
       .send({
-        name: 'Manchester United',
+        name: 'Westbrom Football United',
         manager: 'Robert',
         league: 'EPL',
         fixtures: [
-          { team: 'Chelsea Fc', date: new Date(2019, 3, 15), location: 'home' },
-          { team: 'Arsenal Fc', date: new Date(2019, 4, 15), location: 'home' },
-          { team: 'Liverpool Fc', date: new Date(2019, 5, 15), location: 'away' }
+          { team: '5d4155cfcd68f4086d8df503', date: new Date(2019, 3, 15), location: 'home' },
+          { team: '5d4155cfcd68f4086d8df502', date: new Date(2019, 4, 15), location: 'home' },
+          { team: '5d4155cfcd68f4086d8df506', date: new Date(2019, 5, 15), location: 'away' }
         ]
       });
     expect(res.status).toBe(200);
@@ -161,22 +132,9 @@ describe('PUT ENDPOINT', () => {
 });
 
 describe('DELETE ENDPOINT', () => {
-  let team: ITeam;
-  beforeEach(async () => {
-    team = await new Team({
-      name: 'Manchester United',
-      manager: 'Adedayo ',
-      league: 'EPL',
-      fixtures: [
-        { team: 'Chelsea Fc', date: new Date(2019, 3, 15), location: 'home' },
-        { team: 'Arsenal Fc', date: new Date(2019, 4, 15), location: 'home' }
-      ]
-    }).save();
-  });
-
   it('Should be Defined', async () => {
     const res = await request(app)
-      .delete(`/api/v1/teams/${team._id}`)
+      .delete(`/api/v1/teams/5d4155cfcd68f4086d8df504`)
       .set(
         'Authorization',
         'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNhbWZlb2x1QGdtYWlsLmNvbSIsInBhc3N3b3JkIjoic29tIiwicm9sZSI6IkFETUlOIiwiaWF0IjoxNTY3NTY0MDU5fQ.oj8nb4EOW2ZH7WheIcna0L_s2IM9hOeu46JHRZy5LV0'
@@ -186,12 +144,12 @@ describe('DELETE ENDPOINT', () => {
 
   it('Should Not Contain Deleted Data', async () => {
     await request(app)
-      .delete(`/api/v1/teams/${team._id}`)
+      .delete(`/api/v1/teams/5d4155cfcd68f4086d8df505`)
       .set(
         'Authorization',
         'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNhbWZlb2x1QGdtYWlsLmNvbSIsInBhc3N3b3JkIjoic29tIiwicm9sZSI6IkFETUlOIiwiaWF0IjoxNTY3NTY0MDU5fQ.oj8nb4EOW2ZH7WheIcna0L_s2IM9hOeu46JHRZy5LV0'
       );
-    const res = await Team.findById(team._id);
+    const res = await Team.findById('5d4155cfcd68f4086d8df505');
     expect(res).toBeNull();
   });
 });
