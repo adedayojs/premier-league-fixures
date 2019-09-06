fda2ecf2060d130563699641b348bfb3fdda326d
adedayojs <samfeolu@gmail.com>
2019-09-06 15:49:00 +0100

    Created Data for User Seeds

---
+++ seeder/user.ts
@@ -0,0 +1,101 @@
+import User from '../models/user'
+
+const users = [
+    new User({
+      _id: '5d4155cfcd68f4086d8df490',
+      firstName: 'Izuchukwu',
+      lastName: 'Ogbodo',
+      email: 'izuking@gmail.com',
+      password: 'decagon555',
+      phone: '09023150090',
+      role: 'ADMIN',
+      club:'Manchester United'
+    }),
+    new User({
+        _id: '5d4155cfcd68f4086d8df491',
+        firstName: 'Tolu',
+        lastName: 'Adesina',
+        email: 'info@decagon.institute',
+        password: 'decagon222',
+        phone: '09023150390',
+        role: 'USER',
+        club:'Manchester City'
+    }),
+    new User({
+        _id: '5d4155cfcd68f4086d8df492',
+        firstName: 'Ibrahim',
+        lastName: 'Habu',
+        email: 'ibrahim@gmail.com',
+        password: 'decagon333',
+        phone: '09023150890',
+        role: 'USER',
+        club:'Manchester United'
+    }),
+    new User({
+      _id: '5d4155cfcd68f4086d8df493',
+      firstName: 'Adedayo',
+      lastName: 'Olagunju',
+      email: 'adedayo@gmail.com',
+      password: 'decagon',
+      phone: '09023150090',
+      role: 'ADMIN',
+      club:'Arsenal Fc'
+    }),
+    new User({
+      _id: '5d4155cfcd68f4086d8df494',
+      firstName: 'Cynthia',
+      lastName: 'Alokwu',
+      email: 'cynthia@gmail.com',
+      password: 'decagon',
+      phone: '09023150090',
+      role: 'ADMIN',
+      club:'Chelsea Fc'
+    }),
+    new User({
+      _id: '5d4155cfcd68f4086d8df495',
+      firstName: 'Benjamin',
+      lastName: 'Mark',
+      email: 'benmark@gmail.com',
+      password: 'decagon',
+      phone: '09023150090',
+      role: 'USER',
+    }),
+    new User({
+      _id: '5d4155cfcd68f4086d8df496',
+      firstName: 'Austine',
+      lastName: 'Ogbuanya',
+      email: 'austine@gmail.com',
+      password: 'decagon',
+      phone: '09023150090',
+      role: 'USER',
+    }),
+    new User({
+      _id: '5d4155cfcd68f4086d8df497',
+      firstName: 'Mary',
+      lastName: 'Etokwudo',
+      email: 'mary@gmail.com',
+      password: 'decagon',
+      phone: '09023150090',
+      role: 'ADMIN',
+    }),
+    new User({
+      _id: '5d4155cfcd68f4086d8df498',
+      firstName: 'Abetang',
+      lastName: 'Otondo',
+      email: 'abetang@gmail.com',
+      password: 'decagon',
+      phone: '09023150090',
+      role: 'ADMIN',
+    }),
+    new User({
+      _id: '5d4155cfcd68f4086d8df499',
+      firstName: 'Joseph',
+      lastName: 'Amakiri',
+      email: 'amakiri@gmail.com',
+      password: 'decagon',
+      phone: '09023150090',
+      role: 'ADMIN',
+    }),
+  ];
+  
+  export default users;
