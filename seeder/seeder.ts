f1c83d2e8846c6d647e36ff6dcbbe3f258907989
adedayojs <samfeolu@gmail.com>
2019-09-06 16:47:17 +0100

    Completed Seeder Code, added command for seeding

--- seeder/seeder.ts
+++ seeder/seeder.ts
@@ -1,15 +1,14 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userSeeds from './user'
-import teamSeeds from './teams'
-import fixtureSeeds from './fixtures'
+import userSeeds from './user';
+import teamSeeds from './teams';
+import fixtureSeeds from './fixtures';
 
 dotenv.config();
 
 let env = `${process.env.NODE_ENV}`;
 env = process.env.NODE_ENV || 'DEVELOPMENT';
 let envString = env.toUpperCase();
-
 
 // Connection to mongoDB
 
@@ -29,3 +28,25 @@ connection.on('error', () => {
   console.log('Error Connecting To Database');
 });
 
+const allSeeds: any = [...fixtureSeeds, ...userSeeds, ...teamSeeds];
+
+async function seed() {
+  await mongoose.connection.dropDatabase();
+
+  let counter = 0;
+  for (let index = 0; index < allSeeds.length; index++) {
+    //Saving each Section to database
+    allSeeds[index].save((error: any, _result: any) => {
+      if (error) {
+        console.log(error);
+      }
+
+      counter++;
+      if (counter === allSeeds.length) {
+        //Disconnect if this is the last seed
+        mongoose.disconnect();
+      }
+    });
+  }
+}
+seed();
