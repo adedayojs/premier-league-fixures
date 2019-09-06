8105781a5278ac158acec98d49c884c9eb3cc0ee
adedayojs <samfeolu@gmail.com>
2019-09-06 14:01:01 +0100

    Added Search by query parameters to fixture route

--- routes/fixtures/controller.ts
+++ routes/fixtures/controller.ts
@@ -31,6 +31,18 @@ async function createFixture(req: express.Request, res: express.Response) {
 }
 
 async function viewFixtures(req: express.Request, res: express.Response) {
+  /* Check if there is a search Parameter, if there is search*/
+  if (req.query) {
+    const fixture = await Fixture.find(req.query).catch(err => {
+      res
+        .status(500)
+        .send(err)
+        .end();
+    });
+    res.json(fixture);
+    return;
+  }
+  /*If there is no search parameter*/
   const fixture = await Fixture.find({}).catch(err => {
     res
       .status(500)
